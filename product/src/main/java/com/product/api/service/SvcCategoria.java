0db0afbe048117404dc5268959de00792e8827dc
Jose Luis Flores <jose_luis_flores_gutierrez@ciencias.unam.mx>
2025-10-21 08:39:45 -0500

    feature:entrega practica 6

--- product/src/main/java/com/product/api/service/SvcCategoria.java
+++ product/src/main/java/com/product/api/service/SvcCategoria.java
@@ -1,12 +1,19 @@
 package com.product.api.service;
 
-import com.product.api.entity.Categoria;
 import java.util.List;
 
-import org.springframework.http.ResponseEntity;
+import com.product.api.dto.DtoCategoryIn;
+import com.product.api.entity.Categoria;
+import com.product.commons.ApiResponse;
 
 public interface SvcCategoria {
 
-     public ResponseEntity<List<Categoria>> getCategorias();
-
+     
+     public List<Categoria> findAll();
+     public List<Categoria> findActive();
+     public ApiResponse create(DtoCategoryIn in);
+     public ApiResponse update(DtoCategoryIn in, int id);
+     public ApiResponse enable(int id);
+     public ApiResponse disable(int id);
+     
 }
--- product/src/main/java/com/product/api/service/SvcCategoriaImp.java
+++ product/src/main/java/com/product/api/service/SvcCategoriaImp.java
@@ -2,12 +2,17 @@ package com.product.api.service;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.product.api.dto.DtoCategoryIn;
 import com.product.api.entity.Categoria;
 import com.product.api.repository.RepoCategoria;
+import com.product.commons.ApiResponse;
+import com.product.exception.ApiException;
+import com.product.exception.DBAccessException;
 
 @Service
 public class SvcCategoriaImp implements SvcCategoria{
@@ -16,10 +21,84 @@ public class SvcCategoriaImp implements SvcCategoria{
     RepoCategoria repo;
 
 
-    @Override
-	public ResponseEntity<List<Categoria>> getCategorias() {
-		return new ResponseEntity<>(repo.getCategorias(), HttpStatus.OK);
+
+	@Override
+	public List<Categoria> findAll() {
+		try {
+            return repo.findAll();
+        }catch (DataAccessException e) {
+            throw new DBAccessException();
+        }
 	}
 
+
+	@Override
+	public List<Categoria> findActive() {
+		try {
+            return repo.findActive();
+        }catch (DataAccessException e) {
+            throw new DBAccessException();
+        }
+	}
+
+
+	@Override
+	public ApiResponse create(DtoCategoryIn in) {
+		try {
+            repo.create(in.getCategory(), in.getTag());
+            return new ApiResponse("La categoria ha sido registrada");
+        }catch (DataAccessException e) {
+            if (e.getLocalizedMessage().contains("ux_category"))
+                throw new ApiException(HttpStatus.CONFLICT, "El nombre de la categoria ya está registrado");
+            if (e.getLocalizedMessage().contains("ux_tag"))
+                throw new ApiException(HttpStatus.CONFLICT, "El tag de la categoria ya está registrado");
+            throw new DBAccessException();
+        }
+	}
+
+
+	@Override
+	public ApiResponse update(DtoCategoryIn in, int id) {
+		try {
+            validateId(id);
+            repo.update(id, in.getCategory(), in.getTag());
+            return new ApiResponse("La categoria ha sido actualizada");
+        }catch (DataAccessException e) {
+            if (e.getLocalizedMessage().contains("ux_category"))
+                throw new ApiException(HttpStatus.CONFLICT, "El nombre de la categoria ya está registrado");
+            if (e.getLocalizedMessage().contains("ux_tag"))
+                throw new ApiException(HttpStatus.CONFLICT, "El tag de la categoria ya está registrado");
+            throw new DBAccessException();
+        }
+	}
+
+
+	@Override
+	public ApiResponse enable(int id) {
+		try {
+            validateId(id);
+            repo.setStatus(id, 1);
+            return new ApiResponse("La categoria ha sido activada");
+        }catch (DataAccessException e) {
+            throw new DBAccessException();
+        }
+	}
+
+
+	@Override
+	public ApiResponse disable(int id) {
+		try {
+            validateId(id);
+            repo.setStatus(id, 0);
+            return new ApiResponse("La categoria ha sido desactivada");
+        }catch (DataAccessException e) {
+            throw new DBAccessException();
+        }
+	}
+
+	private void validateId(int id) {
+        if(repo.findById(id).isEmpty())
+            throw new ApiException(HttpStatus.NOT_FOUND, "El id de la categoria no existe");
+    }
 
 }
