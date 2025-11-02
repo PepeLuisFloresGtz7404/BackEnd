209ec853fd3cf80315e0c36a0799276de79f7439
Jose Luis Flores <jose_luis_flores_gutierrez@ciencias.unam.mx>
2025-11-02 17:05:07 -0600

    feat: entrega practica 7

---
+++ product/src/main/java/com/product/api/controller/CtrlProductImage.java
@@ -0,0 +1,52 @@
+package com.product.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.product.api.dto.DtoProductImageIn;
+import com.product.api.dto.DtoProductImageOut;
+import com.product.api.service.SvcProductImage;
+import com.product.commons.ApiResponse;
+import com.product.exception.ApiException;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/product-image")
+public class CtrlProductImage {
+
+    @Autowired
+    SvcProductImage svc;
+
+    
+    @PostMapping("/{id}/image")
+    public ResponseEntity<ApiResponse> createProductImage(@Valid @RequestBody DtoProductImageIn in, BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            throw new ApiException(HttpStatus.BAD_REQUEST, bindingResult.getFieldError().getDefaultMessage());
+
+        return svc.uploadProductImage(in);
+    }
+    
+    @GetMapping("/{id}/image")
+    public ResponseEntity<DtoProductImageOut> getProductImage(@PathVariable Integer id) {
+        return svc.getProductImage(id);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse> disableProduct(@PathVariable Integer id) {
+        return svc.deleteProductImage(id);
+    }
+    @PatchMapping("/{id}")
+    public ResponseEntity<ApiResponse> enableProduct(@PathVariable Integer id) {
+        return svc.enableProductImage(id);
+    }
+}
